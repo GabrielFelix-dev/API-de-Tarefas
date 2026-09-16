90631b6069bdcc8cb2db0567b781cbbbaafbb2a4
GabrielFelix-dev <gabrielfsc832@gmail.com>
2026-09-16 16:14:09 -0300

    feat: CRUD completo Lista de Tarefas

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from typing import List, Optional
 app = FastAPI(title="API de Tarefas")
 
 #MODELO DE DADOS
-class tarefa(BaseModel):
+class Tarefa(BaseModel):
     id: Optional[int] = None
     titulo: str
     descricao: str
@@ -19,3 +19,44 @@ class tarefa(BaseModel):
 #BANCO EM MEMORIA
 db_tarefas = []
 
+
+#ROTA PARA LISTAR TODAS AS TAREFAS
+@app.get("/tarefas", response_model=List[Tarefa])
+async def listar_tarefas():
+    return db_tarefas
+
+#ROTA PARA CRIAR TAREFA
+@app.post("/tarefas", response_model=Tarefa, status_code = 201)
+async def criar_tarefas(tarefa: Tarefa):
+    tarefa.id = len(db_tarefas) + 1
+    db_tarefas.append(tarefa)
+    return tarefa
+
+#ROTA BUSCAR TAREFA POR ID
+@app.get("/tarefas/{tarefa_id}", response_model=Tarefa)
+async def buscar_tarefas(tarefa_id: int):
+    for t in db_tarefas:
+        if t.id == tarefa_id:
+            return t
+    raise HTTPException(status_code=404, detail="Tarefa não encontrada")
+
+#ROTA PARA ATUALIZAR TAREFA POR ID
+@app.put("/tarefas/{tarefa_id}", response_model=Tarefa)
+async def atualizar_tarefas(tarefa_id: int, tarefa_atualizada: Tarefa):
+    for index, t in enumerate(db_tarefas):
+        if t.id == tarefa_id:
+            tarefa_atualizada.id = tarefa_id
+
+            db_tarefas[index] = tarefa_atualizada
+            return tarefa_atualizada
+
+    raise HTTPException(status_code=404, detail="Tarefa não encontrada")
+
+#ROTA PARA DELETAR UMA TAREFA POR ID
+@app.delete("/tarefas/{tarefa_id}", status_code = 204, response_model=Tarefa)
+async def excluir_tarefas(tarefa_id: int):
+    for t in db_tarefas:
+        if t.id == tarefa_id:
+            db_tarefas.remove(t)
+            return t
+    raise HTTPException(status_code=404, detail="Tarefa não encontrada")
